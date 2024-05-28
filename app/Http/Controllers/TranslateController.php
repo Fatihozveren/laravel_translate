63b7f483855507586753fb38b91aa58ef478962f
fatihozveren <fatihozveren8520@hotmail.com>
2024-05-28 21:23:15 +0300

    dsadsa

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -9,34 +9,37 @@ class TranslateController extends Controller
 {
     public static function translate(Request $request){
 
-        $q = $request->arama;
-        $detect_url = 'https://translo.p.rapidapi.com/api/v3/detect?text='.$q;
+            $q = $request->arama;
 
-        $detect_response = Http::withHeaders([
-            'X-RapidAPI-Key' => '083260f6e7msh76b3e8d1341225ep1d1b5djsn38b8a0fe5156',
-            'X-RapidAPI-Host' => 'translo.p.rapidapi.com'
-        ])->withBody('{
-             "text": "'.$q.'",
-            }',"raw")->get($detect_url);
+            $url = 'https://deep-translate1.p.rapidapi.com/language/translate/v2';
 
-        $detect_result = $detect_response['lang'];
-
-
-        $url = 'https://deep-translate1.p.rapidapi.com/language/translate/v2?key='.$q;
-
-        $response = Http::withHeaders([
-            'content-type' => 'application/json',
-            'X-RapidAPI-Key' => 'd783f70ac8msh1d9637566ac541ap123b4ejsnea686ef4b1bf',
-            'X-RapidAPI-Host' => 'deep-translate1.p.rapidapi.com'
-        ])->withBody('{
-             "q": "'.$q.'",
-             "source": "'.$detect_result.'",
-              "target": "tr"
+            $response = Http::withHeaders([
+                'X-RapidAPI-Host' => 'deep-translate1.p.rapidapi.com',
+                'X-RapidAPI-Key' => 'c124051fc5mshcbc605c6a5c7ee3p165f95jsn10cc1040b23c',
+                'content-type' => 'application/json',
+            ])->withBody('{
+                "q": "'.$q.'",
+                "source": "en",
+                "target": "tr"
             }',"raw")->post($url);
 
 
+
+//        $url = 'https://deep-translate1.p.rapidapi.com/language/translate/v2?key='.$q;
+//
+//        $response = Http::withHeaders([
+//            'content-type' => 'application/json',
+//            'X-RapidAPI-Key' => 'd783f70ac8msh1d9637566ac541ap123b4ejsnea686ef4b1bf',
+//            'X-RapidAPI-Host' => 'deep-translate1.p.rapidapi.com'
+//        ])->withBody('{
+//             "q": "'.$q.'",
+//             "source": "'.$detect_result.'",
+//              "target": "tr"
+//            }',"raw")->post($url);
+//
+//
         $result = $response["data"]["translations"]["translatedText"];
 
-        return view('front.search.search', compact('result', 'detect_result'));
+        return view('front.search.search', compact('result'));
     }
 }
